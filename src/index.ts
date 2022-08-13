c531b6bf69910c1801ee44c05d6e3208713c40e5
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-13 23:44:06 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import { Company } from "./Company";
 import { CustomMap } from "./CustomMap";
 import { User } from "./User";
 
-update
 
 
 const myMap: CustomMap = new CustomMap("map");
@@ -10,7 +9,4 @@ const myMap: CustomMap = new CustomMap("map");
 
 myMap.addMarker(new User());
 myMap.addMarker(new Company());
-=======
 new CustomMap("map");
-
- master
