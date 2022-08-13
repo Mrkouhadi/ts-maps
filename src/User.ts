cf5a35fad9d2933de8ced2bf28bcf577571ff13e
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-14 01:15:24 +0800

    Update User.ts

--- src/User.ts
+++ src/User.ts
@@ -1,17 +1,27 @@
 import faker from 'faker/dist/faker.js'
+import { MappableInterface } from './CustomMap';
 
 
-export class User {
+export class User implements MappableInterface{
     name: string;
     location:{
         lat: number,
         lng: number,
     }
+    color: string = 'red';
     constructor(){
         this.name = faker.name.firstName();
         this.location={
             lat:parseFloat(faker.address.latitude()),
             lng:parseFloat(faker.address.longitude()),
         }
+    }
+    popupContent():string{
+        return `
+                <div>
+                    <h1>Hi there ! </h1>
+                    <h3> This is ${this.name}</h1>
+                </div>
+        `;
     }
 }
